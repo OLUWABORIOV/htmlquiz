734bb9ea10ea331a51955c5b7d5443c21a07d5a1
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-07-10 08:18:41 -1200

    (commit)

---
+++ script.js
@@ -0,0 +1,121 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('quetion')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffleQuestions.correctQuestionsIndex;
+let quizScore =8;
+
+
+startButton.addEventListener('click,startGame')
+
+nextButton.addEventListener('click' ,() =>{
+    correctQuestionsIndex++
+    setnextQuestion()
+})
+
+
+function starGame(){
+    startButton.classList.add('hide')
+    shuffleQuestions=question.sort(() =>Math.random() -0.)
+    correctQuestionsIndex=0;
+    questionContainerElement.classList.remove('hide')
+    setnextQuestion()
+    quizScore=0;
+}
+
+function set nextQuestion( {
+    resetState();
+    showQuestion(shuffledQuestions[correctQuestionsIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText= question.question;
+    question.answers.forEach(answer) =>{
+        const button =document.createElement('button')
+        button.innerText=answer,text;
+        button.classList.add('btn')
+        if( answer.correct) {
+            button.dataset.correct =answer.correct
+        }
+        button.addEventListener('click'),selectA appendChild(newChild: HTmlButtonElement): HTmlButtonElement
+        answerButtonElement.appendChild(button)
+    })
+}
+
+function resetState(){
+    clearstatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonElement.firstChild) {
+        answerButtonElement.removeChild(answerButtonElement.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedButton=e.target
+    const correct =selectedButton.dataset.correct
+
+     setStatusClass(document.body,correct)
+     Array.from(answerButtonElement.children).forEach((button)=>{
+         setStatusClass(buton,button.dataseEat.correct) 
+    })
+    if(shuffledQuestions.lenght > currentQuestionIndex +1){
+        nextButton.classList.remove('hide')
+    }else {
+        startButton.innerText ='restart'
+        startButton.classList.remove =("hide")
+    }
+    if(selectedButton.dataset = correct) {
+        quizScore++
+    }
+    document.getElementById('right-answers').innerText=quizScore
+}
+
+function setStatusClass(element,correct){
+    clearStatusClass(Element);
+    if(correct){
+        element.classList.add("correct")
+    }else {
+        element.classList.add('wrong')
+    }
+}
+
+
+
+
+function clearstatusClass[element]{
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const question =[
+    {
+        question: 'which of these is a Javascript framework?',
+        answers :[
+            {Text: 'Python', correct: false},
+            {Text: 'Django', correct: false},
+            {Text: 'React', correct: true},
+            {Text: 'Eclipse', correct: false}
+        ],
+    },
+    {
+        question: 'who is the prime minister of India?',
+        answers :[
+            {Text: 'Narendra Modi', correct: true},
+            {Text: 'Rahul Gandhi', correct: false},
+            
+        ],
+    },
+    {
+        question: 'what is 4*3?',
+        answers :[
+            {Text: '6', correct: false},
+            {Text: '12', correct: false},
+            
+        ],
+    },
+
+]
+            
